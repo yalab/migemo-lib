cf782e0d330a3ee8ad264bb943c227e70ed4bc04
yalab <rudeboyjet@gmail.com>
2011-03-04 10:38:55 +0900

    tests/regex-dict.sh, tests/user-dict.sh is wrong?

--- tests/regex-dict.sh
+++ tests/regex-dict.sh
@@ -20,7 +20,7 @@ cmp tmp.right tmp.out || exit 1
 
 echo "m" | ruby -I.. ../migemo -u $USERDICT -r $REGEXDICT -n -O3 test-dict > tmp.out
 cat <<'EOF' > tmp.right
-[mｍっまみむめもッマミムメモ]|Message Of The Day|\([-0-9a-zA-Z_.]+@[-0-9a-zA-Z_.]+\)
+[mｍっまみむめもッマミムメモ]|Message\ Of\ The\ Day|\([-0-9a-zA-Z_.]+@[-0-9a-zA-Z_.]+\)
 EOF
 cmp tmp.right tmp.out || exit 1
 
--- tests/user-dict.sh
+++ tests/user-dict.sh
@@ -8,7 +8,7 @@ export REGEXDICT=../regex-dict.sample
 
 echo "mot" | ruby -I.. ../migemo -u $USERDICT -n -O3 test-dict > tmp.out
 cat <<'EOF' > tmp.right
-mot|ｍｏｔ|も[たちっつてと]|モ(?:[タチッツテト]|ー(?:ション|ター)|スラ)|Message Of The Day
+mot|ｍｏｔ|も[たちっつてと]|モ(?:[タチッツテト]|ー(?:ション|ター)|スラ)|Message\ Of\ The\ Day
 EOF
 cmp tmp.right tmp.out || exit 1
 
