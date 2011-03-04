a41b6739e68c872c290479b9d6424f5824213e74
yalab <rudeboyjet@gmail.com>
2011-03-04 23:14:17 +0900

    Rewrite to ruby's test/unit tests/regex-dict.sh -> test/regex-dict_test.rb.

---
+++ test/regex-dict.rb
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+require 'test/unit'
+require 'migemo'
+
+class RegexDictTest < Test::Unit::TestCase
+  BASE_DIR = File.dirname(File.expand_path(__FILE__))
+  def setup
+    @dict = MigemoStaticDict.new(BASE_DIR + '/test-dict')
+    @regex_dict = MigemoRegexDict.new(BASE_DIR + '/../data/regex-dict.sample')
+  end
+
+  def test_m
+    migemo = Migemo.new(@dict, 'm')
+    migemo.regex_dict = @regex_dict
+    assert_equal '[mｍっまみむめもッマミムメモ]|\([-0-9a-zA-Z_.]+@[-0-9a-zA-Z_.]+\)', migemo.regex
+  end
+
+
+  def test_ur
+    migemo = Migemo.new(@dict, 'ur')
+    migemo.regex_dict = @regex_dict
+    assert_equal 'ur|ｕｒ|う[っらりるれろ]|ウ[ッラリルレロ]|\(\(http\|https\|ftp\|afs\|wais\|telnet\|ldap\|gopher\|news\|nntp\|rsync\|mailto\)://[-_.!~*\'()a-zA-Z0-9;/?:@&=+$,%#]+\)', migemo.regex
+  end
+
+  def test_m_with_userdict
+    migemo = Migemo.new(@dict, 'm')
+    migemo.regex_dict = @regex_dict
+    migemo.user_dict = MigemoUserDict.new(BASE_DIR + '/../data/user-dict.sample')
+    assert_equal '[mｍっまみむめもッマミムメモ]|Message\ Of\ The\ Day|\([-0-9a-zA-Z_.]+@[-0-9a-zA-Z_.]+\)', migemo.regex
+  end
+end
